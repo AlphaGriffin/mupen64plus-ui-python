49d395b000b7d71abd50ea984c09371af6f5b60d
Eric Petersen <eric.alphagriffin@gmail.com>
2017-02-10 16:26:56 -0700

    fixed line endings

--- src/m64py/frontend/trainer.py
+++ src/m64py/frontend/trainer.py
@@ -74,120 +74,113 @@ class xpad(object):
         pygame.event.pump()
         return self.joystick.get_button(4) == 1
         
-
+x = 0
 class Process(object):
-    
+    def __init__(self):
+        self.test = 1
+        
+        
     def gamepadImageMatcher(path):
-        INRO =\
-        """
-        SAW -- matches gamepad csv data rows to images based on timestamps
-        Returns two matching arrays
-        """
+        #INFO = "SAW -- matches gamepad csv data rows to images based on timestamps\nReturns two matching arrays"
 	
     	  # Open CSV for reading
-    	  csv_path = os.path.join(path, "data.csv")
-    	  print ("about to open: {}".format(csv_path))
-    	  csv_io = open(csv_path, 'r')
-    	  print ("csv successfully opened")
-    	
-    	  # Convert to a true array
-    	  csv = []
-    	  for line in csv_io:
-    		# Split the string into array and trim off any whitespace/newlines in elements
-    		csv.append([item.strip() for item in line.split(',')])
+        csv_path = os.path.join(path, "data.csv")
+        print("about to open: {}".format(csv_path))
+        csv_io = open(csv_path, 'r')
+        print ("csv successfully opened")
+        
+        # Convert to a true array
+        csv = []
+        for line in csv_io:
+            # Split the string into array and trim off any whitespace/newlines in elements
+            csv.append([item.strip() for item in line.split(',')])
+        if not csv:
+            print ("CSV HAS NO DATA")
+            return None, None
+            
+        # Get list of images in directory and sort it
+        all_files = os.listdir(path)
+        print ("all_files: {}".format(all_files))
+        #images = [image for image in all_files if any(image.endswith('.png'))]
+        images = []
+        for filename in all_files:
+            if filename.endswith('.png'):
+                images.append(filename)
+        images = sorted(images)
+        print ("sorted images list: {}".format(images))
+        
+        if not images:
+            print ("FOUND NO IMAGES");
+            return None, None
     
-    	  if not csv:
-    		print ("CSV HAS NO DATA");
-    		return None, None
+        # We're going to build up 2 arrays of matching size:
+        keep_csv = []
+        keep_images = []
     	
-    	# Get list of images in directory and sort it
-    	all_files = os.listdir(path)
-    	print ("all_files: {}".format(all_files))
-    	#images = [image for image in all_files if any(image.endswith('.png'))]
-    	images = []
-    	for filename in all_files:
-    		if filename.endswith('.png'):
-    			images.append(filename)
-    	images = sorted(images)
-    	print ("sorted images list: {}".format(images))
-    
-    	if not images:
-    		print ("FOUND NO IMAGES");
-    		return None, None
-    
-    	# We're going to build up 2 arrays of matching size:
-    	keep_csv = []
-    	keep_images = []
+        # Prime the pump (queue)...
+        prev_line = csv.pop(0)
+        prev_csvtime = int(prev_line[0])
+        print ("line 0: {}".format(prev_line))
     	
-    	# Prime the pump (queue)...
-    	prev_line = csv.pop(0)
-    	prev_csvtime = int(prev_line[0])
-    	print ("line 0: {}".format(prev_line))
+        #prev_image = images[0]
+        #prev_imgtime = 0
     	
-    #            prev_image = images[0]
-    #            prev_imgtime = 0
-    	
-    	# Find next matching image
-    	while images:
-    		imgfile = images[0]
-    		print ("imgfile: {}".format(imgfile))
-    		
-    		# Get image time:
-    		#     Cut off the "gamename-" from the front and the ".png" from the end
-    		hyphen = imgfile.rfind('-') # Get last index of '-'
-    		if hyphen < 0:
-    			print ("UNEXPECTED FILENAME, ABORTING: {}".format(imgfile))
-    			break
-    		imgtime = int(imgfile[hyphen+1:-4]) # cut it out!
-    		print ("imgtime: {}".format(imgtime))
-    		
-    		lastKeptWasImage = False # Did we last keep an image, or a line?
-    	
-    		if imgtime > prev_csvtime:
-    			print ("keeping image: {}".format(imgtime))
-    			keep_images.append(imgfile)
-    			del images[0]
-    			lastKeptWasImage = True
-    			
-    			# We just kept an image, so we need to keep a
-    			# corresponding input row too
-    			while csv:
-    				line = csv.pop(0)
-    				print ("line: {}".format(line))
+        # Find next matching image
+        while images:
+            imgfile = images[0]
+            print ("imgfile: {}".format(imgfile))
+    		 # Get image time:
+    		 #     Cut off the "gamename-" from the front and the ".png" from the end
+            hyphen = imgfile.rfind('-') # Get last index of '-'
+            if hyphen < 0:
+                print ("UNEXPECTED FILENAME, ABORTING: {}".format(imgfile))
+                break
+            imgtime = int(imgfile[hyphen+1:-4]) # cut it out!
+            print ("imgtime: {}".format(imgtime))
+            lastKeptWasImage = False # Did we last keep an image, or a line?
+            if imgtime > prev_csvtime:
+                print ("keeping image: {}".format(imgtime))
+                keep_images.append(imgfile)
+                del images[0]
+                lastKeptWasImage = True
+                
+                
+                # We just kept an image, so we need to keep a
+                #corresponding input row too
+                while csv:
+                    line = csv.pop(0)
+                    print ("line: {}".format(line))
+                    csvtime = int(line[0])
+                    print ("csvtime: {}".format(str(csvtime)))
     				
-    				# Get CSV line timestamp:
-    				csvtime = int(line[0])
-    				print ("csvtime: {}".format(str(csvtime)))
-    				
-    				if csvtime >= imgtime:
-    					# We overshot the input queue... ready to
-    					# keep the previous data line
-    					print ("keeping line: {}".format(prev_csvtime))
-    					keep_csv.append(prev_line)
-    					lastKeptWasImage = False
+                    if csvtime >= imgtime:
+                        # We overshot the input queue... ready to
+                        # keep the previous data line
+                        print ("keeping line: {}".format(prev_csvtime))
+                        keep_csv.append(prev_line)
+                        lastKeptWasImage = False
     					
-    				prev_line = line
-    				prev_csvtime = csvtime
+                        prev_line = line
+                        prev_csvtime = csvtime
     				
-    				if csvtime >= imgtime:
-    					break;
+                        if csvtime >= imgtime:
+                            break;
     					
-    			if not csv:
-    				print ("OUT OF CSV DATA")
-    				if lastKeptWasImage:
-    					print ("keeping line: {}".format(prev_csvtime))
-    					keep_csv.append(prev_line)
-    				break
+                    if not csv:
+                        print ("OUT OF CSV DATA")
+                        if lastKeptWasImage:
+                            print ("keeping line: {}".format(prev_csvtime))
+                            keep_csv.append(prev_line)
+                        break
     				
-    		else:
-    			print ("dropping image: {}".format(imgtime))
-    			del images[0]
+                else:
+                    print ("dropping image: {}".format(imgtime))
+                    del images[0]
     		
-    	print ("len(keep_data): {}".format(len(keep_csv)))
-    	print ("len(keep_images): {}".format(len(keep_images)))
-    #	self.print_console("MATCHED RESULTS COUNT: {} <--> {}".format(len(keep_csv),len(keep_images)))
-    
-    	return keep_csv, keep_images
+        print ("len(keep_data): {}".format(len(keep_csv)))
+        print ("len(keep_images): {}".format(len(keep_images)))
+        #self.print_console("MATCHED RESULTS COUNT: {} <--> {}".format(len(keep_csv),len(keep_images)))
+        return keep_csv, keep_images
         
 class Trainer(AGBlank):
     """AG_Recorder Widget of MuPen64 Python Ui"""
